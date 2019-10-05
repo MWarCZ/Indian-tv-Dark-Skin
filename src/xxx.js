1e3d8cdda560ab5fdeb7f4043943101d174935a1
MWarCZ <mv.deoxys@gmail.com>
2019-10-05 16:42:27 +0200

    Pridany komentare do js souboru.

--- src/xxx.js
+++ src/xxx.js
@@ -1,17 +1,26 @@
 // Project: Indian-tv Dark Skin
 // Filename: src/xxx.js
 
+/**
+ * Nastaveni stavu pro vyber akce prideleni/odebrani tridy dark.
+ * EN: Set status for select action add/remove class (dark).
+ * @param {0|1} value (0=remove, 1=add, other=nothing) 
+ */
 function setStatus(value=1) {
   localStorage['status'] = value;
 }
+/**
+ * Funkce prida nebo odebere tridu (dark) v elementu body dle nastaveneho statusu.
+ * EN: Function adds or removes class (dark) in body element according to set status.
+ */
 function classNow() {
-  var cl = "dark"; 
-  var x = document.body.classList; 
+  var darkSkinClass = "dark"; 
+  var bodyClassList = document.body.classList; 
   if(localStorage.status == 1) {
-    x.add(cl); 
+    bodyClassList.add(darkSkinClass); 
   }
   else if (localStorage.status == 0) {
-    x.remove(cl);
+    bodyClassList.remove(darkSkinClass);
   }
 }
 
@@ -19,7 +28,6 @@ function classNow() {
 if(!localStorage.status) {
   setStatus();
 }
-
 classNow();
 
 // EndOfFile: src/xxx.js
